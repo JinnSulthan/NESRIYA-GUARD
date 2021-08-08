c3ede2caac56e8eca32c299c67efe1790baf6576
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-08-08 21:13:36 +0530

    NAZRIMOL❣️

--- main_startup/config_var.py
+++ main_startup/config_var.py
@@ -43,7 +43,7 @@ class Config(object):
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     STRINGSESSION = os.environ.get("STRINGSESSION", None)
     ASSISTANT_START_PIC = os.environ.get(
-        "ASSISTANT_START_PIC", "https://telegra.ph//file/0a4e8046cfe782ae7c1b7.jpg"
+        "ASSISTANT_START_PIC", "https://telegra.ph//file/9dd271d3a066b459d4cde.jpg"
     )
     STRINGSESSION_2 = os.environ.get("STRINGSESSION_2", None)
     STRINGSESSION_3 = os.environ.get("STRINGSESSION_3", None)
@@ -62,10 +62,10 @@ class Config(object):
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     FBAN_GROUP = int(os.environ.get("FBAN_GROUP", False))
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/DevsExpo/FridayUserbot"
+        "UPSTREAM_REPO", "https://github.com/MR-JINN-OF-TG/NESRIYA-GUARD"
     )
     ALIVE_IMG = os.environ.get(
-        "ALIVE_IMG", "https://telegra.ph//file/0a4e8046cfe782ae7c1b7.jpg"
+        "ALIVE_IMG", "https://telegra.ph//file/9dd271d3a066b459d4cde.jpg"
     )
     U_BRANCH = "master"
     HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
